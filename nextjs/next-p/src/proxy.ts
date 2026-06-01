b088b217f6f4ff53f25066df1b2d02f6821fa7a5
sayantanpal9 <sayantanpal2006.24@kgpian.iitkgp.ac.in>
2026-06-01 13:49:09 +0530

    completed the next auth part, now time for api and page making

--- nextjs/next-p/src/proxy.ts
+++ nextjs/next-p/src/proxy.ts
@@ -1,19 +1,30 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-export { default } from 'next-auth/middleware'
-import { getToken } from 'next-auth/jwt'
- 
-// This function can be marked `async` if using `await` inside
-export async function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
-}
- 
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
+
 export const config = {
-    matcher: [
-        '/signIn',
-        '/signUp',
-        '/dashboard',
-        '/',
-        '/verify/:path*'
-  ]
+  matcher: ['/dashboard/:path*', '/signIn', '/signUp', '/', '/verify/:path*'],
+};
+
+export async function proxy(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/signIn') ||
+      url.pathname.startsWith('/signUp') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/signIn', request.url));
+  }
+
+  return NextResponse.next();
 }
